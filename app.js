d7fa7271f0993ec49c0946625b4e29c76403c1aa
NabilMezher <nabilmezher2002@gmail.com>
2023-02-14 21:18:46 +0200

    Added the listings for residents page

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const { errorHandler } = require('./middlewares/errorMiddleware')
+const express = require("express");
+const { errorHandler } = require("./middlewares/errorMiddleware");
 
-const app = express()
+const app = express();
 
 // Welcome message to our API
-app.get('/', (req, res) => {
-   res.status(200).json({ message: 'Welcome to Badde Salemtak API Service' })
-})
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Welcome to Badde Salemtak API Service" });
+});
 
 // Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/users', require('./routes/userRoute'))
-app.use('/api/traveler', require('./routes/travelerListingRoute'))
+app.use("/api/users", require("./routes/userRoute"));
+app.use("/api/traveler", require("./routes/travelerListingRoute"));
+app.use("/api/resident", require("./routes/residentListingRoute"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
