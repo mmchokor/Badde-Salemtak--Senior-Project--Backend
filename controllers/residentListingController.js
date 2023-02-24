937ec5af2a04adf687168117e7085ebc44871330
mmchokor <mohameddd701@gmail.com>
2023-02-24 21:50:24 +0200

    added getting each image for each  listing

--- controllers/residentListingController.js
+++ controllers/residentListingController.js
@@ -2,10 +2,15 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const ResidentListing = require('../models/residentListingModel')
 const APIFeatures = require('../utils/apiFeatures')
-const { PutObjectCommand } = require('@aws-sdk/client-s3')
 const crypto = require('crypto')
 
-const { S3Client } = require('@aws-sdk/client-s3')
+// s3 configuration
+const {
+   S3Client,
+   PutObjectCommand,
+   GetObjectCommand,
+} = require('@aws-sdk/client-s3')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
@@ -19,6 +24,7 @@ const s3 = new S3Client({
       secretAccessKey,
    },
 })
+// end s3 configuration
 
 const generateFileName = (bytes = 32) =>
    crypto.randomBytes(bytes).toString('hex')
@@ -33,6 +39,17 @@ const getRListings = asyncHandler(async (req, res) => {
       .limitFields()
       .paginate()
    const residentListings = await features.query
+
+   // fetching a temporary signed url for each image of each listing
+   for (const listing of residentListings) {
+      const getObjectParams = {
+         Bucket: bucketName,
+         Key: listing.imageCover,
+      }
+      const command = new GetObjectCommand(getObjectParams)
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
+      listing.imageCover = url
+   }
 
    res.status(200).json({
       status: 'success',
@@ -60,6 +77,22 @@ const getRListing = asyncHandler(async (req, res) => {
       res.status(404)
       throw new Error('Resident Listing not found')
    }
+
+   const signedImagesURL = []
+
+   // fetching a signeed url for each image of the listing and storing it in an array
+   for (const image of residentListing.images) {
+      const getObjectParams = {
+         Bucket: bucketName,
+         Key: image,
+      }
+      const command = new GetObjectCommand(getObjectParams)
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
+	  signedImagesURL.push(url)
+   }
+   // then we update the listing images with the signed urls
+   residentListing.images = signedImagesURL
+
    res.status(200).json({
       status: 'success',
       data: {
