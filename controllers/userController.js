aaef106dc6029b3ef544e00195242317c725ec26
mmchokor <mohameddd701@gmail.com>
2023-03-29 16:40:03 +0200

    made the domain specified in the .env

--- controllers/userController.js
+++ controllers/userController.js
@@ -107,7 +107,7 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
 
       //now we should send it to the user's email
 
-      const resetURL = `https://badde-salemtak-api.vercel.app/api/users/resetPassword/${resetToken}`
+      const resetURL = process.env.BACKEND_DOMAIN + `/api/users/resetPassword/${resetToken}`
 
       //the resetPassword is another function implemented under this one
 
