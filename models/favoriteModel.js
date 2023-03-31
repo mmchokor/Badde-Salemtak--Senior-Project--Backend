ce9a2302b40af3ff1bfe5ba69f386e6293b8e3e3
NabilMezher <nabilmezher2002@gmail.com>
2023-03-31 14:57:43 +0300

    Added timestamp for Favorite

--- models/favoriteModel.js
+++ models/favoriteModel.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const favoritesSchema = new mongoose.Schema({
-   user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-   },
-   listing: {
-      type: mongoose.Schema.Types.ObjectId,
-      refPath: 'listingType',
-      required: true,
-   },
-   listingType: {
-      type: String,
-      enum: ['residentListing', 'traverlerListing'],
-      required: true,
-   },
-})
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	listing: {
+		type: mongoose.Schema.Types.ObjectId,
+		refPath: "listingType",
+		required: true,
+	},
+	listingType: {
+		type: String,
+		enum: ["residentListing", "traverlerListing"],
+		required: true,
+	},
+	timestamp: { type: Date, default: Date.now },
+});
 
-const Favorites = mongoose.model('Favorites', favoritesSchema)
+const Favorites = mongoose.model("Favorites", favoritesSchema);
 
-module.exports = Favorites
+module.exports = Favorites;
