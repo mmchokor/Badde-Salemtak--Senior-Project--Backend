e1da6485da6f0e72f16cbfe78acf4d3c78b69bd3
mmchokor <mohameddd701@gmail.com>
2023-04-29 11:49:39 +0300

    Front Issues Resolved - Img Cover & att for trav

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -2,12 +2,35 @@ const asyncHandler = require('express-async-handler')
 const Favorites = require('../models/favoriteModel')
 const User = require('../models/userModel')
 
+// s3 configuration
+const {
+   S3Client,
+   PutObjectCommand,
+   GetObjectCommand,
+   DeleteObjectCommand,
+} = require('@aws-sdk/client-s3')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY_VERCEL
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+
+const s3 = new S3Client({
+   region,
+   credentials: {
+      accessKeyId,
+      secretAccessKey,
+   },
+})
+// end s3 configuration
+
 // @desc    create favorite resident listings
 // @route   post /api/favorite/
 // @access  Private
 const createFavorite = asyncHandler(async (req, res) => {
    const { user, listing, listingType } = req.body
-   console.log(req.body)
+
    if (!user || !listing || !listingType) {
       res.status(400)
       throw new Error('The request is missing required parameters')
@@ -34,11 +57,32 @@ const getFavoritesByUser = asyncHandler(async (req, res) => {
       user: req.params.userId,
    }).populate({
       path: 'listing',
+      populate: {
+         path: 'user',
+         select: 'firstname lastname _id',
+      },
    })
 
    if (!favoriteListings) {
       res.status(404)
       throw new Error('No favorite listings found')
+   }
+
+   // fetching a temporary signed url for each image of each listing
+   for (const favorite of favoriteListings) {
+      if (favorite.listingType === 'residentListing') {
+         if (favorite.listing.imageCover) {
+            const getObjectParams = {
+               Bucket: bucketName,
+               Key: favorite.listing.imageCover,
+            }
+            const command = new GetObjectCommand(getObjectParams)
+            const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
+            favorite.listing.imageCover = url
+         } else {
+            favorite.listing.imageCover = ''
+         }
+      }
    }
 
    res.status(200).json({
